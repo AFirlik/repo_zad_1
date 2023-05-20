c0c9cd0d825368864b43154fefb2dce1224f1402
Anita Firlik <71291@student.ue.poznan.pl>
2023-05-20 20:27:48 +0200

    Zadanie 1.2

--- solo-work/solo_1.py
+++ solo-work/solo_1.py
@@ -6,3 +6,8 @@ student = "Ola"
 # wykorzystaj w princie zmienne hello i student
 x = "{} {}".format(hello, student)
 print(x)
+
+# zadanie 1.2
+
+student = input("Wpisz swoje imie: ")
+print("Hello " + student)
