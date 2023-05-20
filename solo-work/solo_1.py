bce144a5638b4afb95d339d07f36f59df09c34e3
Anita Firlik <71291@student.ue.poznan.pl>
2023-05-20 21:54:22 +0200

    Zadanie 1.10

--- solo-work/solo_1.py
+++ solo-work/solo_1.py
@@ -70,3 +70,39 @@ for i in range(len(nazwiska)):
     if nazwiska[i].startswith("N"):
         liczba_n += 1
 print("Liczba studentow na N wynosi: " + str(liczba_n))
+
+# zadanie 1.10
+
+wykres_1 = [[2, 4], [4, 4], [6, 4]]
+wykres_2 = [[2, 3], [4, 4], [6, 5]]
+wykres_3 = [[2, 3], [4, 3], [5, 4]]
+def is_linear(points):
+    x_values = [point[0] for point in points]
+    y_values = [point[1] for point in points]
+    if len(set(y_values)) == 1:
+        return True
+    slope = (y_values[1] - y_values[0]) / (x_values[1] - x_values[0])
+    for i in range(2, len(points)):
+        new_slope = (y_values[i] - y_values[0]) / (x_values[i] - x_values[0])
+        if new_slope != slope:
+            return False
+    return True
+
+wykres_1_funkcja_liniowa = is_linear(wykres_1)
+wykres_2_funkcja_liniowa = is_linear(wykres_2)
+wykres_3_funkcja_liniowa = is_linear(wykres_3)
+
+if wykres_1_funkcja_liniowa:
+    print("Dla punktow w wykres_1 mozna wyznaczyc funkcje liniowa.")
+else:
+    print("Dla punktow w wykres_1 nie mozna wyznaczyc funkcji liniowej.")
+
+if wykres_2_funkcja_liniowa:
+    print("Dla punktow w wykres_2 mozna wyznaczyc funkcje liniowa.")
+else:
+    print("Dla punktow w wykres_2 nie mozna wyznaczyc funkcji liniowej.")
+
+if wykres_3_funkcja_liniowa:
+    print("Dla punktow w wykres_3 mozna wyznaczyc funkcje liniowa.")
+else:
+    print("Dla punktow w wykres_3 nie mozna wyznaczyc funkcji liniowej.")
